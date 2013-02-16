39784de1e89021754716b603ff2e2d9cfca9dfa4
Romildo Paiter <romildopaiter@gmail.com>
2013-02-15 21:28:04 -0300

    Entidade Jogador

---
+++ PW2Sockets/src/ufmt/entidades/Jogador.java
@@ -0,0 +1,17 @@
+package ufmt.entidades;
+
+public class Jogador {
+
+	private String nome;
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
